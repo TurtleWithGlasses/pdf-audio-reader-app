7c0d202056bb898f732987a21fd424ec3a1ac059
Mehmet Ali Soylu <mhmtsoylu1928@gmail.com>
2023-07-28 15:47:51 +0300

    Change page size, improve extract text function

--- pdf_audio_reader.py
+++ pdf_audio_reader.py
@@ -6,33 +6,29 @@ import pyttsx3
 
 def extract_text():
     file = filedialog.askopenfile(parent=root, mode="rb", title="Choose a PDF file")
-    if file != None:
-        pdfreader = PyPDF2.PdfFileReader(file)
+    if file is not None:
+        pdfreader = PyPDF2.PdfReader(file)
         global text_extracted
         text_extracted = ""
-        for page_num in range(pdfreader.numPages):
-            page_object = pdfreader.getPage(page_num)
+        for page_num in range(len(pdfreader.pages)):
+            page_object = pdfreader.pages[page_num]
             text_extracted += page_object.extract_text()
         file.close()
 
-def speak_text():
-    global rate
-    global male
-    global female
+def speak_text(text, rate, male, female):
     rate = int(rate.get())
     engine.setProperty("rate", rate)
-    male = int(male.get())
-    female = int(female.get())
     all_voices = engine.getProperty("voices")
     male_voice = all_voices[0].id
     female_voice = all_voices[1].id
-    if male == 0 and female == 0 or male == 1 and female == 1:
+    if male.get() == 1 and female.get() == 0:
         engine.setProperty("voice", male_voice)
-    elif male == 0 and female == 1:
+    elif female.get() == 1 and male.get() == 0:
         engine.setProperty("voice", female_voice)
     else:
         engine.setProperty("voice", male_voice)
-    engine.say(text_extracted)
+
+    engine.say(text)
     engine.runAndWait()
 
 
@@ -41,11 +37,11 @@ def stop_speaking():
 
 
 def Application(root):
-    root.geometry("{}x{}".format(700, 600))
+    root.geometry("{}x{}".format(1000, 1000))
     root.resizable(width=False, height=False)
     root.title("PDF 2 Audio")
     root.configure(background="light green")
-    global rate, male, female
+
     frame1 = Frame(root, width=500, height=200, bg="indigo")
     frame2 = Frame(root, width=500, height=450, bg="indigo")
     frame1.pack(side="top", fill="both")
@@ -56,35 +52,38 @@ def Application(root):
     name2 = Label(frame1, text="Listen to Your PDF File", fg="black", bg="blue", font="Arial 28 bold")
     name2.pack()
 
-    button = Button(frame2, text="Select PDF File", activeforeground="red", command=extract_text(), padx=70, pady=20, fg="white", bg="black",
-                    font="Arial 28 bold")
+    button = Button(frame2, text="Select PDF File", activeforeground="red", command=lambda: extract_and_speak(), padx=70, pady=20, fg="white", bg="black", font="Arial 28 bold")
     button.grid(row=0, pady=20, columnspan=2)
 
-    rate_text = Label(frame2, text="Enter Rate of Speech",fg="black", bg="blue", font="Arial 28")
+    rate_text = Label(frame2, text="Enter Rate of Speech", fg="black", bg="blue", font="Arial 28")
     rate_text.grid(row=1, column=0, pady=15, padx=0, sticky=W)
-    rate = Entry(frame2, text="200", fg="black", bg="blue", font="Arial 28")
-    rate.grid(row=1, column=1, padx=30, pady=15, sticky=W)
+    rate_entry = Entry(frame2, text="200", fg="black", bg="blue", font="Arial 28")
+    rate_entry.grid(row=1, column=1, padx=30, pady=15, sticky=W)
 
     voice_text = Label(frame2, text="Select Voice", fg="black", bg="blue", font="Arial 28")
     voice_text.grid(row=2, column=0, pady=15, padx=0, sticky=E)
 
-    male = IntVar()
-    male_opt = Checkbutton(frame2, text="Male", fg="black", bg="blue", font="Arial 28", variable=male, onvalue=1, offvalue=0)
+    male_var = IntVar()
+    male_opt = Checkbutton(frame2, text="Male", fg="black", bg="blue", font="Arial 28", variable=male_var, onvalue=1, offvalue=0)
     male_opt.grid(row=2, column=1, pady=0, padx=30, sticky=W)
-    female = IntVar()
-    female_opt = Checkbutton(frame2, text="Female", fg="black", bg="blue", font="Arial 28", variable=female, onvalue=1, offvalue=0)
+
+    female_var = IntVar()
+    female_opt = Checkbutton(frame2, text="Female", fg="black", bg="blue", font="Arial 28", variable=female_var, onvalue=1, offvalue=0)
     female_opt.grid(row=3, column=1, pady=0, padx=30, sticky=W)
 
-    submit_button = Button(frame2, text="Play PDF File", command=speak_text(), activeforeground="red", padx=60, pady=10, fg="white", bg="black",
-                    font="Arial 28 bold")
-    submit_button.grid(row=4, pady=65, colum=0)
-    stop_button = Button(frame2, text="Stop Playing", command=stop_speaking(), activeforeground="red", padx=60, pady=10, fg="white", bg="black",
-                    font="Arial 28 bold")
+    submit_button = Button(frame2, text="Play PDF File", command=lambda: speak_text(text_extracted, rate_entry, male_var, female_var), activeforeground="red", padx=60, pady=10, fg="white", bg="black", font="Arial 28 bold")
+    submit_button.grid(row=4, pady=65, column=0)
+
+    stop_button = Button(frame2, text="Stop Playing", command=stop_speaking, activeforeground="red", padx=60, pady=10, fg="white", bg="black", font="Arial 28 bold")
     stop_button.grid(row=4, column=1, pady=65)
 
+def extract_and_speak():
+    global text_extracted
+    text_extracted = extract_text()
+    if text_extracted:
+        speak_text(text_extracted, rate, male, female)
 
 if __name__ == "__main__":
-    my_text, rate, male, female = "", 100, 0, 0
     engine = pyttsx3.init()
     root = Tk()
     Application(root)
